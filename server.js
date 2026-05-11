6b310716bca10d7a36777164e123da53931af8db
Ktoyura <uraspiridonov109@gmail.com>
2026-05-11 10:17:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const io = new Server(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] }
 });
 
+// CORS for HTTP routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
